eeb8e35a4fd2082564d4ad3bdb1fc11a66bf638d
tinaungmoe <tinaung0933@gmail.com>
2020-11-24 13:51:17 +0630

    while loop testing

--- javascript.js
+++ javascript.js
@@ -1,5 +1,20 @@
-var num = 2;
-while(num!=Infinity) {
-    console.log(num + " * " + num + " = " + num * num)
-    num = (num*num);
+var cars = ["BMW","VIGO","CROWN"];
+
+var links = [
+    "https://www.youtube.com/",
+    "https://www.facebook.com/",
+    "https://www.google.com.mm/webhp?tab=rw"];
+
+var i = 0;
+var numOfcar = cars.length;
+var numOflink = links.length;
+var text = "";
+
+
+while(i<numOfcar){
+    text += "<li><a href='"+links[i]+"'>"+cars[i]+"</a></li>" ;
+    i++;
 }
+
+
+document.getElementById("demo3").innerHTML = text;
